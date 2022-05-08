2e674fcad4ddf6701f24719d0ee060e3929a7619
Thalles <thallicuto3@gmail.com>
2022-05-08 02:54:47 -0300

    Update Home

--- crud/frontend/src/components/template/footer.jsx
+++ crud/frontend/src/components/template/footer.jsx
@@ -3,9 +3,10 @@ import './footer.css'
 export default props =>
     <footer className="footer">
         <span>
-            Desenvolvido com <i className='fa fa-heart text-success'></i> por
-            <strong>T<span className='text-success'>h</span>a
+            Desenvolvido por
+            <strong> T<span className='text-success'>h</span>a
             <span className='text-success'>l</span>l
-            <span className='text-success'>e</span>s</strong>
+            <span className='text-success'>e</span>s </strong>
+            <i className='fa fa-heart text-success'></i>
         </span>
     </footer>
